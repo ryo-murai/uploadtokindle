e8f4fa50a7e4371bfe3f4090d4c3dd6c7c8e0cae
ryo_murai <murai.ry@gmail.com>
2012-06-07 08:35:27 +0900

    just migrate obtaining info from properties file to dao

---
+++ uploadtokindle/src/main/java/org/ry0mry/uploadtokindle/dataaccess/UploadUserDao.java
@@ -0,0 +1,40 @@
+package org.ry0mry.uploadtokindle.dataaccess;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.ry0mry.uploadtokindle.controller.UploadController;
+import org.ry0mry.uploadtokindle.model.UploadUser;
+
+import com.google.appengine.api.users.User;
+
+public class UploadUserDao {
+	public UploadUser getUploadUser(User user) {
+		//return Datastore.get(UploadUser.class, KeyFactory.createKey("UploadUser", user.getUserId()));
+
+		try {
+			UploadUser uploadUser = new UploadUser();
+			Properties props = new Properties();
+			props.load(UploadController.class.getResourceAsStream("/secret.properties"));
+			
+			String sender = props.getProperty("mail.sender");
+			String recipient = props.getProperty("mail.recipient");
+			
+			if(!user.getEmail().equals(sender)) {
+				return null;
+			}
+			
+			uploadUser.setUploadDestAddress(recipient);
+			uploadUser.setUser(user);
+			
+			return uploadUser;
+		} catch (IOException e) {
+			return null;
+		}
+	}
+	
+	public void updateUploadUser(UploadUser user) {
+		// TODO: do nothgin for mean time
+		//Datastore.put(user);
+	}
+}
