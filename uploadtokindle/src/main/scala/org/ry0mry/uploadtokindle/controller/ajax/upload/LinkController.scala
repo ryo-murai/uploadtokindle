be6e4665869882c3e2e42a3195b3f948d8f34918
ryo-murai <murai.ry@gmail.com>
2012-06-14 18:06:16 +0900

    add nature of scala

---
+++ uploadtokindle/src/main/scala/org/ry0mry/uploadtokindle/controller/ajax/upload/LinkController.scala
@@ -0,0 +1,11 @@
+package org.ry0mry.uploadtokindle.controller.ajax.upload
+
+import org.slim3.controller.Controller
+import org.slim3.controller.Navigation
+
+class LinkController extends Controller {
+
+    override def run() =  {
+        forward("/")
+    }
+}
